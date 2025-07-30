3225ee610b683970a81ec54c662245e721727990
ryan <wyrms01@outlook.com>
2025-07-30 17:17:53 +0800

    基础使用

--- consumer/src/main/java/com/ryan/consumer/listeners/MqListener.java
+++ consumer/src/main/java/com/ryan/consumer/listeners/MqListener.java
@@ -1,19 +1,35 @@
 package com.ryan.consumer.listeners;
 
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.Exchange;
+import org.springframework.amqp.rabbit.annotation.Queue;
+import org.springframework.amqp.rabbit.annotation.QueueBinding;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MqListener {
 
-    @RabbitListener(queues = "hello.queue1")
+    @RabbitListener(
+            bindings = @QueueBinding(
+                    value = @Queue(name = "hello.queue1", durable = "true"),
+                    exchange = @Exchange(name = "hello.direct", type = ExchangeTypes.DIRECT),
+                    key = {"a", "b"}
+            )
+    )
     public void receiveMessage(String message) throws InterruptedException {
-        System.out.println("Received message: " + message);
-        Thread.sleep(50);
+        System.out.println("C1-Received message: " + message);
     }
 
-    @RabbitListener(queues = "hello.queue1")
+
+    @RabbitListener(
+            bindings = @QueueBinding(
+                    value = @Queue(name = "hello.queue2", durable = "true"),
+                    exchange = @Exchange(name = "hello.direct", type = ExchangeTypes.DIRECT),
+                    key = {"b", "c"}
+            )
+    )
     public void receiveMessage2(String message) {
-        System.err.println("Received message: " + message);
+        System.err.println("C2-Received message: " + message);
     }
 }
--- publisher/src/test/java/com/ryan/publisher/PublisherApplicationTests.java
+++ publisher/src/test/java/com/ryan/publisher/PublisherApplicationTests.java
@@ -8,17 +8,36 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PublisherApplicationTests {
 
-	@Autowired
-	RabbitTemplate rabbitTemplate;
+    @Autowired
+    RabbitTemplate rabbitTemplate;
 
-	@Test
-	void contextLoads() {
-		String key="hello.queue1";
-		for(int i = 0; i < 50; i++) {
-			String message = "Hello World_" + i;
-			rabbitTemplate.convertAndSend(key, message);
-			System.out.println("Sent message: " + message);
-		}
-	}
+    @Test
+    void contextLoads() {
+        String key = "hello.queue1";
+        for (int i = 0; i < 50; i++) {
+            String message = "Hello World_" + i;
+            rabbitTemplate.convertAndSend(key, message);
+            System.out.println("Sent message: " + message);
+        }
+    }
 
+    @Test
+    void sendToFanout() {
+        String exchange = "hello.fanout";
+        String message = "Hello World to Exchange";
+        for (int i = 0; i < 50; i++) {
+            rabbitTemplate.convertAndSend(exchange, "", message + " " + i);
+            System.out.println("Sent message to exchange: " + message + " " + i);
+        }
+    }
+
+    @Test
+    void sendToDirect() {
+        String exchange = "hello.direct";
+        rabbitTemplate.convertAndSend(exchange, "a", "Key A Message");
+        rabbitTemplate.convertAndSend(exchange, "b", "Key B Message");
+        rabbitTemplate.convertAndSend(exchange, "c", "Key C Message");
+        System.out.println("Sent messages to direct exchange with keys 'a' and 'b'");
+
+    }
 }
